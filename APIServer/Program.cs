a0ce3bf5b64c36ec826345f8706177ee6ba4e2ce
DESKTOP-L353D9C\Dmitry <vibro@mail.ru>
2024-03-09 01:35:58 +0300

    Initial comit

---
+++ APIServer/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using ClassLibraryAPI;
+
+namespace ServerCore
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+//        start:
+
+            string ip = "127.0.0.1";
+            int port = 8080;
+
+            ServerLog.New();
+
+            HttpListener listener = new HttpListener();
+            listener.Prefixes.Add("http://" + ip + ":" + port + "/");
+            ServerLog.Created(ip, port);
+
+            listener.Start();
+            ServerLog.Started(ip, port);
+
+            Task.Run(() =>
+            {
+                while (true)
+                {
+                    HttpListenerContext context = listener.GetContext();
+                    HttpListenerRequest request = context.Request;
+                    HttpListenerResponse response = context.Response;
+
+                    string ResponseString = "";
+                    if (request.HttpMethod == "GET")
+                    {
+                        ResponseString = "GET request received";
+                        ServerLog.Request(request.Headers.ToString());
+                        ServerLog.Response(ResponseString);
+                    }
+                    else if (request.HttpMethod == "POST")
+                    {
+                        byte[] data;
+                        using (var reader = new System.IO.StreamReader(request.InputStream, request.ContentEncoding))
+                        {
+                            data = Encoding.UTF8.GetBytes(reader.ReadToEnd());
+                        }
+                        ResponseString = Encoding.UTF8.GetString(data);
+                        ServerLog.Request(request.Headers.ToString());
+                        ServerLog.Response(ResponseString);
+                    }
+                    else
+                    {
+                        ResponseString = "Unsupported method: " + request.HttpMethod;
+                        ServerLog.Request(request.Headers.ToString());
+                        ServerLog.Error(ResponseString);
+                    }
+
+                    byte[] buffer = Encoding.UTF8.GetBytes(ResponseString);
+                    response.ContentType = "text/html";
+                    response.ContentLength64 = buffer.Length;
+                    response.OutputStream.Write(buffer, 0, buffer.Length);
+                    response.Close();
+                }
+            });
+
+            ServerLog.Break();
+
+            listener.Stop();
+            ServerLog.Stopped();
+        }
+    }
+}
---
+++ APIServer/ServerCore/ServerLog.cs
@@ -0,0 +1,63 @@
+﻿using ClassLibraryAPI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace ServerCore
+{
+    public static class ServerLog
+    {
+        public static void New()
+        {
+            Log.New("Server");
+            Log.Header("API Server");
+        }
+
+        public static void Created(string ip, int port)
+        {
+            Log.Note("API server created as Server on {0}:{1}", ip, port);
+            Log.Warning("API server starting...");
+        }
+
+        public static void Started(string ip, int port)
+        {
+            Log.SetCursorToPreviousLine();
+            Log.ClearLine();
+            Log.Warning("API server on {0}:{1} started", ip, port);
+            Log.Warning("API server works...");
+        }
+
+        public static void Request(string Text)
+        {
+            Log.Success("API server received request from Client: {0}", Text);
+            Log.Line();
+        }
+
+        public static void Response(string Text)
+        {
+            Log.Success("API server is sending responce to Client: {0}", Text);
+            Log.Line();
+        }
+
+        public static void Break()
+        {
+            Log.Warning("Press Enter to stop API Server");
+            Log.Line();
+            Log.ReadLine();
+        }
+
+        public static void Stopped()
+        {
+            Log.Warning("API Server stopped");
+        }
+
+        public static void Error(string Text)
+        {
+            Log.Error(Text);
+        }
+    }
+
+}
